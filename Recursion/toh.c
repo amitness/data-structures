ded9b79207e6cc24b37de32005530cbf2f6045db
Amit Chaudhary <studenton.com@gmail.com>
2016-07-28 15:53:20 +0545

    Add Tower of Hanoi

---
+++ Recursion/toh.c
@@ -0,0 +1,21 @@
+// Tower of hanoi
+
+#include <stdio.h>
+
+void tower(int n, char from, char to, char aux) {
+    if(n == 1) {
+        printf("Move disk %d from %c to %c.\n", n, from, to);
+        return;
+    }
+    tower(n-1,from, aux, to);
+    printf("Move disk %d from %c to %c.\n", n, from, to);
+    tower(n-1,aux, to, from);
+}
+
+int main() {
+    int n;
+    printf("Enter the number of disks:");
+    scanf("%d", &n);
+    tower(n, 'A', 'C', 'B');
+    return 0;
+}
