70df6361545d73e16544932c9b4fbda3c78e9d6b
Amit Chaudhary <studenton.com@gmail.com>
2016-07-28 15:54:04 +0545

    Add fibonacci series

---
+++ Recursion/fibonacci.c
@@ -0,0 +1,18 @@
+// Fibonacci series using recusion.
+
+#include <stdio.h>
+
+int fib(int n) {
+    if(n<2)
+        return n;
+    return fib(n-1) + fib(n-2);
+}
+
+int main() {
+    int n,i;
+    printf("Enter the number of terms:");
+    scanf("%d", &n);
+    for(i=0;i<n;i++)
+        printf("%d\t", fib(i));
+    return 0;
+}
